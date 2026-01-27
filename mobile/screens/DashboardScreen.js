0f8f5b2397f284cffeef1b4a06ab38dd5ba1c199
Danny Gonzalez <ldanielgg@hotmail.com>
2026-01-26 23:43:34 -0500

    add dashboard and profile screens

---
+++ mobile/screens/DashboardScreen.js
@@ -0,0 +1,9 @@
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+
+export default function Dashboard({ navigation }) {
+  return (
+    <ScrollView>
+      <Text>Dashboard Screen</Text>
+    </ScrollView>
+  );
+}
---
+++ mobile/screens/ProfileScreen.js
@@ -0,0 +1,9 @@
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+
+export default function Profile({ navigation }) {
+  return (
+    <ScrollView>
+        <Text>Profile Screen</Text>
+    </ScrollView>
+  );
+}
